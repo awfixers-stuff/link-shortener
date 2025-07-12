f100047b1713d9f531cac0ac2d8286bec5fc453f
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-11 18:48:35 -0700

    feat: protect routes

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { getSessionCookie } from "better-auth/cookies";
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request, { cookiePrefix: "awfixer" });
+
+  if (!sessionCookie) {
+    console.log("No Cookie");
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
