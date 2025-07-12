4f0273cf1c8c15fb320e43a95f9fe19ba3299a67
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-11 18:45:24 -0700

    chore: config next

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,10 @@
 import type { NextConfig } from "next";
+import { createJiti } from "jiti";
+import { fileURLToPath } from "node:url";
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+jiti.esmResolve("./src/lib/env");
 
 const nextConfig: NextConfig = {
   /* config options here */
