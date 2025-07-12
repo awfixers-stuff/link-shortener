f507f7ee2476aa7e0ddf65c6ffb04d3adb03a5ff
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-12 01:49:30 -0700

    chore: add env variables

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,11 +6,17 @@ export const env = createEnv({
     BETTER_AUTH_SECRET: z.string(),
     BETTER_AUTH_URL: z.string().url(),
     NEON_POSTGRES_DB_URL: z.string(),
+    STRIPE_SECRET_KEY: z.string(),
+    STRIPE_WEBHOOK_SECRET: z.string(),
+    UPLOADTHING_TOKEN: z.string(),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().url(),
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
   },
 });
