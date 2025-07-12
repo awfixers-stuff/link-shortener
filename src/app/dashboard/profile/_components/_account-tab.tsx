0e4d7fcf71f6906fff11d27ef406b4405fbd3c1c
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-12 02:46:37 -0700

    feat: add profile page

---
+++ src/app/dashboard/profile/_components/_account-tab.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { TabsContent } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export function AccountTab() {
+  return (
+    <TabsContent value="account">
+      <div className="bg-background mb-8 rounded-lg border p-6 shadow-sm">
+        <div className="mb-4 text-lg font-semibold">Pro Plan Benefits</div>
+        <div className="mb-6 grid grid-cols-1 gap-4 md:grid-cols-3">
+          <div className="bg-primary-foreground rounded-md p-4">
+            <div className="mb-2 font-medium">Up to 50 Links</div>
+            <div className="text-muted-foreground text-xs">
+              Effortlessly create and manage up to 50 shortened links, perfect
+              for sharing and tracking.
+            </div>
+          </div>
+          <div className="bg-primary-foreground rounded-md p-4">
+            <div className="mb-2 font-medium">Enhanced Analytics</div>
+            <div className="text-muted-foreground text-xs">
+              Dive deeper into your data with enhanced analytics, providing
+              richer insights into your link performance.
+            </div>
+          </div>
+          <div className="bg-primary-foreground rounded-md p-4">
+            <div className="mb-2 font-medium">Priority Support</div>
+            <div className="text-muted-foreground text-xs">
+              Get faster responses and dedicated assistance from the AW&F team
+              whenever you need help!
+            </div>
+          </div>
+        </div>
+        <Button className="mb-2 w-full max-w-xs">Manage Subscription</Button>
+      </div>
+      <div className="bg-background rounded-lg border p-6 shadow-sm">
+        <div className="mb-2 text-lg font-semibold text-red-600">
+          Danger Zone
+        </div>
+        <div className="text-muted-foreground mb-4 text-sm">
+          Permanently delete your account and all associated data.
+        </div>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="destructive">Delete Account</Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Sorry to see go. We will add this functionality soon.</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    </TabsContent>
+  );
+}
---
+++ src/app/dashboard/profile/_components/_keyboard-shortcuts.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import KeyboardShortcut from "@/components/ui/keyboard-shortcut";
+import { useState, useEffect } from "react";
+
+function getCmdOrCtrl() {
+  if (typeof window === "undefined") return "⌘";
+  const platform = window.navigator.platform.toLowerCase();
+  if (platform.includes("mac")) return "⌘";
+  return "Ctrl";
+}
+
+export function KeyboardShortcuts() {
+  const [cmdOrCtrl, setCmdOrCtrl] = useState("⌘");
+  useEffect(() => {
+    setCmdOrCtrl(getCmdOrCtrl());
+  }, []);
+
+  return (
+    <div className="mt-4 flex flex-col gap-2">
+      <div className="text-md font-medium">Keyboard Shortcuts</div>
+      <div className="flex flex-col gap-1 text-base">
+        <div className="flex justify-between">
+          <span>Toggle Sidebar</span>
+          <KeyboardShortcut keys={[getCmdOrCtrl(), "B"]} />
+        </div>
+      </div>
+    </div>
+  );
+}
