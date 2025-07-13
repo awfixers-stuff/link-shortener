9fca0d92c6b9b3b7865622366695bcec7c899bf4
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-13 06:19:28 -0700

    feat: extract table from page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,27 +2,9 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { trpc } from "../server/trpc/server";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
-import { env } from "@/lib/env";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
-import { MoreVerticalIcon, Trash2Icon } from "lucide-react";
+import { LinksTable } from "@/lib/types/database";
 import { getUserLimits } from "@/lib/access-control";
+import { LinksDataTable } from "./_components/_links-table";
 
 export default async function DashboardPage() {
   const session = await auth.api.getSession({ headers: await headers() });
@@ -45,65 +27,7 @@ export default async function DashboardPage() {
       )}
       <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl px-4 py-2 md:min-h-min">
         {data?.links ? (
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="">URL</TableHead>
-                <TableHead>Source</TableHead>
-                <TableHead># of Visits</TableHead>
-                <TableHead>Created At</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead className="w-[50px]" />
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {data.links.map((link) => (
-                <TableRow key={link.id}>
-                  <TableCell>
-                    <Link href={`${env.BETTER_AUTH_URL}/${link.key}`}>
-                      {link.key}
-                    </Link>
-                  </TableCell>
-                  <TableCell>
-                    <Link href={link.destination}>{link.destination}</Link>
-                  </TableCell>
-                  <TableCell>{link.visits}</TableCell>
-                  <TableCell>{link.createdAt}</TableCell>
-                  <TableCell>
-                    <span
-                      className={cn(
-                        "inline-flex items-center rounded-full px-2 py-1 text-xs font-medium",
-                        link.status === "online"
-                          ? "bg-green-300 text-green-700 dark:bg-green-700/20 dark:text-green-500"
-                          : "bg-red-300 text-red-700 dark:bg-red-700/20 dark:text-red-500",
-                      )}
-                    >
-                      {link.status}
-                    </span>
-                  </TableCell>
-                  <TableCell>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button
-                          variant="ghost"
-                          size="icon"
-                          className="size-8 p-0"
-                        >
-                          <MoreVerticalIcon className="size-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end">
-                        <DropdownMenuItem className="text-destructive focus:text-destructive">
-                          <Trash2Icon className="mr-1 size-4" />
-                          Delete
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          <LinksDataTable links={data.links} />
         ) : (
           <span>
             No Shortened URLs found. Start by creating your first shortened link
