a25c0918eeea62838ad3105cc5fd57c652a5b6ca
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-13 06:17:12 -0700

    feat: add dep envs

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,19 +4,24 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     BETTER_AUTH_SECRET: z.string(),
-    BETTER_AUTH_URL: z.string().url(),
+    BETTER_AUTH_URL: z.url(),
     NEON_POSTGRES_DB_URL: z.string(),
     STRIPE_SECRET_KEY: z.string(),
     STRIPE_WEBHOOK_SECRET: z.string(),
     UPLOADTHING_TOKEN: z.string(),
+    RESEND_API_KEY: z.string(),
+    UPSTASH_REDIS_REST_URL: z.string(),
+    UPSTASH_REDIS_REST_TOKEN: z.string(),
   },
   client: {
-    NEXT_PUBLIC_APP_URL: z.string().url(),
+    NEXT_PUBLIC_APP_URL: z.url(),
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
+    NEXT_PUBLIC_RESEND_FROM: z.email(),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_RESEND_FROM: process.env.NEXT_PUBLIC_RESEND_FROM,
   },
 });
