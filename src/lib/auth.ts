7e018e8642396b2d87ccafd66f8c64ac648f9250
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-13 06:18:16 -0700

    feat: update auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,19 @@
-import { betterAuth } from "better-auth";
+import { APIError, betterAuth } from "better-auth";
 import { env } from "./env";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
-import { openAPI, username, haveIBeenPwned, admin } from "better-auth/plugins";
+import {
+  openAPI,
+  username,
+  haveIBeenPwned,
+  admin,
+  createAuthMiddleware,
+} from "better-auth/plugins";
 import { nextCookies } from "better-auth/next-js";
 import Stripe from "stripe";
 import { stripe } from "@better-auth/stripe";
+import { resendClient as resend } from "./resend";
+import { redisServer as redis } from "./redis";
 
 const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2025-06-30.basil",
@@ -18,19 +26,77 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  secondaryStorage: {
+    get: async (key: string) => {
+      const value = await redis.get(key);
+      return typeof value === "string"
+        ? value
+        : value
+          ? JSON.stringify(value)
+          : null;
+    },
+    set: async (key: string, value: string, ttl?: number) => {
+      if (ttl) await redis.set(key, value, { ex: ttl });
+      else await redis.set(key, value);
+    },
+    delete: async (key: string) => {
+      await redis.del(key);
+    },
+  },
   emailAndPassword: {
     enabled: true,
-    requireEmailVerification: false,
-    autoSignIn: true,
+    requireEmailVerification: true,
     async sendResetPassword(data, request) {
-      // TODO: Add RESEND or alternative
+      await resend.emails.send({
+        from: `AWFixer Links <${env.NEXT_PUBLIC_RESEND_FROM}>`,
+        to: data.user.email,
+        subject: "Reset your password",
+        html: `
+          <h2>Reset your password</h2>
+          <p>Click the link below to reset your password:</p>
+          <a href="${data.url}">${data.url}</a>
+          <p>If you didn't request this, you can safely ignore this email.</p>
+        `,
+      });
     },
   },
   emailVerification: {
     sendOnSignUp: true,
+    autoSignInAfterVerification: true,
     expiresIn: 600,
+    async sendVerificationEmail(data, request) {
+      const verificationURL = `${env.NEXT_PUBLIC_APP_URL}/server/api/auth/verify-email?token=${data.token}&callbackURL=/dashboard`;
+
+      await resend.emails.send({
+        from: `AWFixer Links <${env.NEXT_PUBLIC_RESEND_FROM}>`,
+        to: data.user.email,
+        subject: "Verify your AWFixer Links account",
+        html: `
+          <h2>Verify your AWFixer Links Account</h2>
+          <p>Click the link below to verify your email address:</P>
+          <a href="${verificationURL}">${verificationURL}</a>
+        `,
+      });
+    },
   },
   user: {
+    deleteUser: {
+      enabled: true,
+      async sendDeleteAccountVerification(data, request) {
+        const verificationURL = data.url;
+
+        await resend.emails.send({
+          from: `AWFixer Links <${env.NEXT_PUBLIC_RESEND_FROM}>}`,
+          to: data.user.email,
+          subject: "Delete your AWFixer Links account",
+          html: `
+            <h2>Delete your AWFixer Links Account</h2>
+            <p>Click the link below to delete your account:</p>
+            <a href="${verificationURL}">${verificationURL}</a>
+          `,
+        });
+      },
+    },
     additionalFields: {
       newsletter: {
         type: "boolean",
@@ -70,8 +136,17 @@ export const auth = betterAuth({
   session: {
     cookieCache: {
       enabled: false,
-      maxAge: 5 * 60,
+      maxAge: 60 * 60 * 24 * 30,
     },
+    expiresIn: 60 * 60 * 24 * 30,
+    updateAge: 60 * 60 * 24 * 3,
+  },
+  onAPIError: {
+    onError: (err) => {
+      console.error("[BETTER_AUTH] API Error", err);
+    },
+    errorURL: `${env.NEXT_PUBLIC_APP_URL}/auth/sign-in`,
+    throw: true,
   },
   plugins: [
     admin(),
@@ -100,8 +175,11 @@ export const auth = betterAuth({
   ],
   advanced: {
     cookiePrefix: "awfixer",
+    ipAddress: {
+      disableIpTracking: false, // Disable?
+    },
   },
-  trustedOrigins: [env.NEXT_PUBLIC_APP_URL],
+  trustedOrigins: [env.NEXT_PUBLIC_APP_URL, "http://localhost:3000"],
   logger: {
     disabled: false,
     level: "debug",
