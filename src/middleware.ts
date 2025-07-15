15fc7f149858a9dd346127dcb9c679a36ab1ea8f
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-15 00:32:37 -0700

    fix: switch back to using cookie cache

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,11 @@
-import { getSessionCookie } from "better-auth/cookies";
+import { getCookieCache } from "better-auth/cookies";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request, { cookiePrefix: "awfixer" });
-  // const sessionCookie = getCookieCache(request, { cookiePrefix: "awfixer" });
+  // const sessionCookie = getSessionCookie(request, { cookiePrefix: "awfixer" });
+  const sessionCookie = getCookieCache(request, { cookiePrefix: "awfixer" });
 
   if (!sessionCookie) {
-    console.log("No Cookie");
     return NextResponse.redirect(new URL("/auth/sign-in", request.url));
   }
 
