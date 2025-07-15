459822aef12a18c6161ab7c92b430bdf625c5779
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-15 00:35:31 -0700

    feat: enable color scheme provider globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { ColorSchemeProvider } from "@/providers/color-scheme-provider";
 import { TRPCProvider } from "@/providers/trpc-provider";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { fileRouter } from "./server/api/uploadthing/core";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,18 +34,20 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          enableSystem={false}
-          attribute="class"
-          defaultTheme="dark"
-          disableTransitionOnChange
-        >
-          <TRPCProvider>
-            <NextSSRPlugin routerConfig={extractRouterConfig(fileRouter)} />
-            {children}
-          </TRPCProvider>
-          <Toaster richColors />
-        </ThemeProvider>
+        <ColorSchemeProvider>
+          <ThemeProvider
+            enableSystem={false}
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+          >
+            <TRPCProvider>
+              <NextSSRPlugin routerConfig={extractRouterConfig(fileRouter)} />
+              {children}
+            </TRPCProvider>
+            <Toaster richColors />
+          </ThemeProvider>
+        </ColorSchemeProvider>
       </body>
     </html>
   );
