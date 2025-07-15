43d5ca50989ab01642c55182248ebbc5b21ad7d4
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-15 00:35:43 -0700

    feat: update db tyles

--- src/lib/types/database.ts
+++ src/lib/types/database.ts
@@ -3,5 +3,5 @@ import * as Schema from "@/lib/db/schema";
 export type LinksTable = typeof Schema.link.$inferSelect;
 export type UsersTable = typeof Schema.user.$inferSelect;
 export type AccountsTable = typeof Schema.account.$inferSelect;
-export type SessionsTable = typeof Schema.session.$inferSelect;
+export type SubscriptionsTable = typeof Schema.subscription.$inferSelect;
 export type VerificationsTable = typeof Schema.verification.$inferSelect;
