cd6fb72d3884fef132676ed31c9a315b7bb39c70
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-15 00:35:59 -0700

    fix: bug with fetching user limits

--- src/lib/access-control.ts
+++ src/lib/access-control.ts
@@ -1,10 +1,21 @@
-import { authClient } from "./auth-client";
+"use server";
+
+import { headers } from "next/headers";
+import { auth } from "./auth";
 
 export const getUserLimits = async () => {
-  const { data: subscriptions } = await authClient.subscription.list();
-  const activeSub = subscriptions?.find((sub) => sub.status === "active");
-  const links = activeSub?.limits?.links ?? 5;
-  const analytics = activeSub?.limits?.analytics ?? 1;
+  try {
+    const subscriptions = await auth.api.listActiveSubscriptions({
+      headers: await headers(),
+    });
+    const activeSub = subscriptions?.find((sub) => sub.status === "active");
 
-  return { links, analytics };
+    const links = activeSub?.limits?.links ?? 5;
+    const analytics = activeSub?.limits?.analytics ?? 1;
+
+    return { links, analytics };
+  } catch (err) {
+    console.error("[ERROR] Fetching User Limits", err);
+    return { links: 5, analytics: 1 };
+  }
 };
