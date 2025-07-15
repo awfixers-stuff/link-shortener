67fcb23175b67db8f2466f0cb0231e75c25b07bf
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-15 00:34:31 -0700

    feat: add new env variables

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -15,11 +15,13 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.url(),
+    NEXT_PUBLIC_STRIPE_PORTAL_LINK: z.url(),
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
     NEXT_PUBLIC_RESEND_FROM: z.email(),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_STRIPE_PORTAL_LINK: process.env.NEXT_PUBLIC_STRIPE_PORTAL_LINK,
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     NEXT_PUBLIC_RESEND_FROM: process.env.NEXT_PUBLIC_RESEND_FROM,
