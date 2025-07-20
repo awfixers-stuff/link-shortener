d57f409fd503299e1b1bd50fee783ba0cc0eefc6
Kourosh Alasti <coding@kouroshalasti.com>
2025-07-20 02:45:25 -0700

    feat: add new temp pages

---
+++ src/app/dashboard/profile/_components/_contact-tab.tsx
@@ -0,0 +1,10 @@
+import { TabsContent } from "@/components/ui/tabs";
+
+export function ContactTab() {
+  return (
+    <TabsContent value="contact-us">
+      <h2>Contact Us</h2>
+      <p className="text-muted-foreground">Coming Soon!</p>
+    </TabsContent>
+  )
+}
---
+++ src/app/dashboard/profile/_components/_settings-tab.tsx
@@ -0,0 +1,10 @@
+import { TabsContent } from "@/components/ui/tabs";
+
+export function SettingsTab() {
+  return (
+    <TabsContent value="settings">
+      <h1>Edit Profile</h1>
+      <p className="text-muted-foreground">Coming Soon!</p>
+    </TabsContent>
+  )
+}
